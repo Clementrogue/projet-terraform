3d8d7fcadcfd66f356acade95c00682ae61b5207
Kowabunga <prunechoc2142@gmail.com>
2026-03-11 16:20:23 +0100

    fix: ajout des declarations de variables manquantes

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
 variable "public_subnets" {
   type = list(string)
 }
@@ -8,6 +17,11 @@ variable "private_subnets" {
 
 variable "app_ami_id" {
   type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
 }
 
 variable "iam_instance_profile_name" {
