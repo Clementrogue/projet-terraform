f87c90f861aa521333592c88187f54fe02a02c70
Kowabunga <prunechoc2142@gmail.com>
2026-03-12 11:29:02 +0100

    feat: optimisation de l'ASG pour valider le test de charge en lab

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+cat <<EOF > main.tf
 resource "aws_security_group" "alb_sg" {
   name   = "alb-security-group"
   vpc_id = var.vpc_id
@@ -48,16 +49,16 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id = var.vpc_id
+  vpc_id   = var.vpc_id
 
   health_check {
     path                = "/"
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 30
+    interval            = 15
     timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
@@ -87,16 +88,18 @@ resource "aws_launch_template" "app_lt" {
 }
 
 resource "aws_autoscaling_group" "app_asg" {
-  name                = "app-asg"
-  vpc_zone_identifier = var.private_subnets
-  target_group_arns   = [aws_lb_target_group.app_tg.arn]
-  min_size            = 2
-  max_size            = 4
-  desired_capacity    = 2
+  name                      = "app-asg"
+  vpc_zone_identifier       = var.private_subnets
+  target_group_arns         = [aws_lb_target_group.app_tg.arn]
+  min_size                  = 2
+  max_size                  = 4
+  desired_capacity          = 2
+  health_check_grace_period = 60
+  default_cooldown          = 60
 
   launch_template {
     id      = aws_launch_template.app_lt.id
-    version = "$Latest"
+    version = "\$Latest"
   }
 }
 
@@ -109,6 +112,7 @@ resource "aws_autoscaling_policy" "cpu_tracking" {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
-    target_value = 50.0
+    target_value = 20.0
   }
 }
+EOF
